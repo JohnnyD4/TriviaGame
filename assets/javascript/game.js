3e50aee84aad7d1967dc68d8a5df015d007086c6
John Davis <johnsdavis95@gmail.com>
2017-05-31 10:03:39 -0400

    Got question one and timer working to go to #2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,66 +8,91 @@ $(document).ready(function() {
 
 		setTimeout(question1, 100);
 
-		setTimeout(timeUp, 1000 * 5);
+		// setTimeout(timeUp, 1000 * 5);
 	})
 
+	var counter = 0;
 	var correctAnswers = 0;
 	var wrongAnswers = 0;
+	var trueAnswers = ["1983", "Mark Richt"];
 	var qAndA = [
 		first = {
-			'quest1': "What year did the Miami Hurricanes win their first National Championship?",
-			'ans1': "<button class='ansButton false'>1911</button>",
-			'ans2': "<button class='ansButton true'>1983</button>",
-			'ans3': "<button class='ansButton false'>1987</button>",
-			'ans4': "<button class='ansButton false'>1999</button>"
+			'quest': "What year did the Miami Hurricanes win their first National Championship?",
+			'ans1': "<button class='ansButton>1911</button>",
+			'ans2': "<button class='ansButton'>1983</button>",
+			'ans3': "<button class='ansButton'>1987</button>",
+			'ans4': "<button class='ansButton'>1989</button>",
+			// 'div': "#answerDiv"
 		},
 	
 		second = {
-			quest2: "What year did the Miami Hurricanes win their first National Championship?",
-			ans2: "<button class='ansButton' value='false'>1911</button>",
-			ans2: "<button class='ansButton' value='true'>1983</button>",
-			ans2: "<button class='ansButton' value='false'>1987</button>",
-			ans2: "<button class='ansButton' value='false'>1999</button>"
+			'quest': "Who is the current Head Coach?",
+			'ans': "<button class='ansButton'>Antonio Conte</button>",
+			'ans': "<button class='ansButton'>Jimmy Johnson</button>",
+			'ans': "<button class='ansButton'>Howard Schnellenberger</button>",
+			'ans': "<button class='ansButton'>Mark Richt</button>"
 	
 		},
 	]
 
-	$(".false").val("false");
-	$(".true").val(1);
 
-	// for (var i = 0; i < qAndA.length; i++) {	
-
-		function question1() {
-
-
-			$(".questions").html(qAndA[0].quest1);
-			 
-			$(".answers").html(qAndA[0].ans1);
-			$(".answers").append(qAndA[0].ans2);
-			$(".answers").append(qAndA[0].ans3);
-			$(".answers").append(qAndA[0].ans4);
-
-
-
-			$(".ansButton").on("click", function() {
-				console.log(this.value);
-
-				// timeUp().hide();
-
-				if (".ansButton".value === 1) {
-				$(".answers").html("Correct!");
-				} else {
-				$(".answers").html("Wrong!");
-			}
-			})
-			
-
-		}
 
 		function timeUp() {
 			$(".questions").html("You ran out of time!");
 			$(".answers").html("Be faster next time");
 		}
-	// }
+
+		function inBetween() {
+			$(".questions").html(qAndA[counter].quest);
+			
+			$(".answers").html(qAndA[counter].ans1);
+			$(".answers").append(qAndA[counter].ans2);
+			$(".answers").append(qAndA[counter].ans3);
+			$(".answers").append(qAndA[counter].ans4);
+		}
+
+		function question1() {
+
+			for (var i = 0; i < qAndA.length; i++) {
+
+				$(".questions").html(qAndA[counter].quest);
+				 
+				$(".answers").html(qAndA[counter].ans1);
+				$(".answers").append(qAndA[counter].ans2);
+				$(".answers").append(qAndA[counter].ans3);
+				$(".answers").append(qAndA[counter].ans4);
+
+			};	
+
+
+
+				$(".ansButton").on("click", function() {
+					
+					var userButton = $(this).text();
+					// timeUp().hide();
+
+					if (userButton == trueAnswers[0]) {
+					$(".answers").html("Correct!");
+					correctAnswers++;
+					counter++;
+
+					} else {
+					$(".answers").html("Wrong!");
+					counter++;
+					wrongAnswers++;
+					}
+
+					setTimeout(inBetween, 1000 * 5);
+
+				})
+				
+
+			
+
+		console.log(correctAnswers);
+		console.log(wrongAnswers);
+	
+
+	}
 	
 })
